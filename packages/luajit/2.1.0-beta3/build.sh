e313660fce7babd02339001143270d9bf3a4e4fe
Gen isys <17355439+guh0613@users.noreply.github.com>
2025-06-23 23:20:53 +0800

    pkg(luajit-2.1.0-beta3): Added luajit 2.1.0-beta3

---
+++ packages/luajit/2.1.0-beta3/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+PREFIX=$(realpath $(dirname "$0"))
+
+mkdir -p build
+cd build
+
+# Download LuaJIT source
+curl -L "https://github.com/LuaJIT/LuaJIT/archive/refs/tags/v2.1.0-beta3.tar.gz" -o luajit.tar.gz
+
+tar xzf luajit.tar.gz --strip-components=1
+rm luajit.tar.gz
+
+make PREFIX="$PREFIX" -j$(nproc)
+make install PREFIX="$PREFIX" -j$(nproc)
+
+cd ..
+rm -rf build
